9b766ad1a2bbaaf7be1bad21e8eb6c12cea11ac4
Luis Gabriel <gabrielset20@outlook.com>
2023-12-09 16:05:46 -0400

    Adição de funcionalidades de Service e Controller para negar e cancelar compromissos

--- src/main/java/com/dra/frontend/controller/CompromissoController.java
+++ src/main/java/com/dra/frontend/controller/CompromissoController.java
@@ -2,7 +2,10 @@ package com.dra.frontend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dra.frontend.service.CompromissoService;
 
@@ -14,5 +17,17 @@ public class CompromissoController {
 
     @Autowired
     CompromissoService compromissoService;
-    
+	
+    @PostMapping("/negar")
+    public String negarCompromisso(@RequestParam("id") long id) {
+        compromissoService.negarCompromisso(id);
+        return "redirect:/compromisso";
+    }
+
+    @PutMapping("/cancelar")
+    public String cancelarCompromisso(@RequestParam("id") long id) {
+        compromissoService.cancelarCompromisso(id);
+        return "redirect:/compromisso";
+    }
+	
 }
--- src/main/java/com/dra/frontend/service/CompromissoService.java
+++ src/main/java/com/dra/frontend/service/CompromissoService.java
@@ -1,8 +1,22 @@
 package com.dra.frontend.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompromissoService {
-    
+	
+	private final String url = "http://localhost:8080/compromisso"; 
+	
+    public void negarCompromisso(long id) {
+        RestTemplate restTemplate = new RestTemplate();
+        String urlNegar = url + "/negar/" + Long.toString(id);
+        restTemplate.postForObject(urlNegar, null, Void.class);
+    }
+
+    public void cancelarCompromisso(long id) {
+        RestTemplate restTemplate = new RestTemplate();
+        String urlCancelar = url + "/cancelar/" + Long.toString(id);
+        restTemplate.put(urlCancelar, null);
+    }
 }
