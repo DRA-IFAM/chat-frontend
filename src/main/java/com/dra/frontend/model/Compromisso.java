353da31c30d37db7d614b81c042e148c06035e8a
Luis Gabriel <gabrielset20@outlook.com>
2023-12-12 17:56:51 -0400

    Funcionalidades de negar e cancelar compromisso

--- src/main/java/com/dra/frontend/model/Compromisso.java
+++ src/main/java/com/dra/frontend/model/Compromisso.java
@@ -1,14 +1,61 @@
 package com.dra.frontend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@Data
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.*;
+
+import lombok.*;
 @NoArgsConstructor
-@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "compromise")
 public class Compromisso {
-    private String nome;
 
-    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	private Contato criador;
+
+	@Column(nullable = false)
+	private String titulo;
+
+	@Column(nullable = false)
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private LocalDateTime data;
+
+	@Column(nullable = false)
+	private String local;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	@ManyToMany
+	private List<Contato> participantes;
+
+	@Enumerated(EnumType.STRING)
+	private Status status;
+
+	public Compromisso() {
+	}
+
+	public Compromisso(Long id, Contato criador, String titulo, LocalDateTime data, String local, String descricao,
+			List<Contato> participantes, Status status) {
+		this.id = id;
+		this.criador = criador;
+		this.titulo = titulo;
+		this.data = data;
+		this.local = local;
+		this.descricao = descricao;
+		this.participantes = participantes;
+		this.status = status;
+	}
+
+	public enum Status {
+		ACEITO, NEGADO, CANCELADO, SOLICITADO, REAGENDADO, CONCLUIDO
+	}
 }
